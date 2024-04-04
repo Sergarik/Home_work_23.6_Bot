efa4f713e7667fb6c7e7f2fb71d3c6631bcf394f
Sergarik <154251262+Sergarik@users.noreply.github.com>
2024-04-04 17:19:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '7015824287:AAEps_r8W06u8Yw_070eZfxyfYbkUDDYJdY'
+
+keys = {'рубль': 'RUB',
+        'доллар': 'USD',
+        'евро': 'EUR',
+        'юань': 'CNY',
+        'йена': 'JPY',
+}
